3830162546f816079a17d756b7282f118e22010a
SPiercer <snowpiercer2001@gmail.com>
2023-05-16 13:19:52 +0300

    removed endpoints

--- lib/src/constants/endpoints.dart
+++
@@ -1,5 +0,0 @@
-abstract class EndPoints {
-  static String baseUrl = "http://backend.staging.sitech.sitech-sites.me";
-  static String authpoint =
-      "https://keycloak.staging.sitech.sitech-devops.com/realms/sitech/protocol/openid-connect/token";
-}
